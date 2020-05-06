4c731b9f66c72bed44c5658d75425f7ddb6d3ce3
M Gough <6558104+MGough@users.noreply.github.com>
2020-05-06 11:53:26 +0100

    link to issue in healthcheck TODO

--- business/services/health_service.py
+++ business/services/health_service.py
@@ -6,4 +6,5 @@ blueprint = Blueprint("health", __name__, url_prefix="/health")
 @blueprint.route("/", methods=["GET"])
 def healthcheck():
     # TODO: Update this to also report on its own dependencies
+    # TODO: See https://github.com/MGough/flask-microservice-sqlalchemy-marshmallow/issues/3
     return jsonify(healthy=True), 200
